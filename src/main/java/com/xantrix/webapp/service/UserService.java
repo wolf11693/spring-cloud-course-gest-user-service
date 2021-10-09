ccef005d8bf334439c94b7ad1b02c67496029918
wolf1193 <roberto.amato.wolf.11.93@gmail.com>
2021-10-09 11:53:10 +0200

    refactor

--- src/main/java/com/xantrix/webapp/service/UserService.java
+++ src/main/java/com/xantrix/webapp/service/UserService.java
@@ -3,25 +3,12 @@ package com.xantrix.webapp.service;
 import java.util.List;
 
 import com.xantrix.webapp.model.User;
-import com.xantrix.webapp.service.exception.DeleteException;
-import com.xantrix.webapp.service.exception.SaveException;
-import com.xantrix.webapp.service.exception.UpdateException;
 
-public interface UserService {
+public interface UserService extends GenericService<User, String> {
 
-	public List<User> getAll();
-	
-	public User getById(String id);
-	
 	public User getByUsername(String username);
-	
+
 	public User getByUsernameAndPassword(String username, String password);
-	
+
 	public List<User> getByRole(String role);
-	
-	public User save(User user) throws SaveException;
-	
-	public void update(String userIdToUpd, User user) throws UpdateException;
-	
-	public void delete(String idUserToDel) throws DeleteException;
 }
